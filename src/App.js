1947774c70e0bef8dbd2a42e379e57b4ff3d6467
Shubham Ghaytadkar <shubhamghaytadkar177@gmail.com>
2026-04-06 16:42:01 +0530

    Fix OAuth redirect URI for production

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from "react";
 function App() {
   const handleLogin = () => {
     const clientId = "3MVG9rZjd7MXFdLgAFuHnK2J5ZsRE4vLIK9Nme3gpdOzdoo9I.PNk4Bba89izbGznNc62bUGdkfd.kg02y1tl";
-    const redirectUri = "http://localhost:3000/callback";
+    const redirectUri = "https://salesforce-validation-rule-manager-pi.vercel.app/callback";
 
     const authUrl =
       `https://login.salesforce.com/services/oauth2/authorize` +
